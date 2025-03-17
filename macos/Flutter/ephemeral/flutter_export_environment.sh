fd9ca47df7acf72003a01a2ea8dd2c2fdb0f1df6
saifbataineh <114767100+saifbataineh@users.noreply.github.com>
2025-03-17 17:20:22 +0300

    new branch setup

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\github\osamaProject\flutter_windows_3.27.3-stable\flutter"
+export "FLUTTER_ROOT=D:\development\mobile\flutter\flutter"
 export "FLUTTER_APPLICATION_PATH=D:\projects\food_tek"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NAME=1.0.1"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
